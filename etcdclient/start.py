6b010180790d0aecdc7537dc9833326b5b2e6ae1
Jacob Repp <jacobrepp@gmail.com>
2016-11-29 00:24:14 -0800

    Adding etcdclient test

---
+++ etcdclient/start.py
@@ -0,0 +1,27 @@
+import etcd
+import sys
+import os
+import pprint
+
+def print_tree(t):
+    pp = pprint.PrettyPrinter(indent=4)
+    pp.pprint(t)
+
+def main():
+    addr = os.getenv('ETCD_ADDR')
+    port = int(os.getenv('ETCD_PORT'))
+
+    if not addr or not port:
+        print 'usage: ETCD_ADDR=<addr> ETCD_PORT=port ./start'
+        sys.exit(1)
+
+    client = etcd.Client(host=addr, port=port)
+
+    key = '/'
+    try:
+        t = client.read(key, recursive=True)
+        print_tree(t)
+    except etcd.EtcdKeyNotFound:
+        print "key not found:", key
+
+if __name__ == '__main__': main()
