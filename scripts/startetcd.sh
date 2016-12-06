c85513b1356fbef1316ce6031e76c5504148ab70
Jacob Repp <jacobrepp@gmail.com>
2016-12-06 08:24:17 -0800

    adding env vars for etcdclient

--- scripts/startetcd.sh
+++ scripts/startetcd.sh
@@ -16,6 +16,8 @@ docker run -d \
 # Start the local etcd client container
 docker run -it \
     --rm \
+    -e "ETCDCTL_API=3" \
+    -e "ETCDCTL_ENDPOINTS=etcd-master:2379" \
     -h "etcd-client" \
     -v "/home/jrepp/containers/etcdclient:/code" \
     --link etcd-master \
